42d4599d691d6675b3dd08ef6bab0c357ae8f38a
Alexander V. Pavlovcik <alexander@pavlovcik.com>
2021-01-28 04:50:36 -0600

    kovan

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # uad-ui
+
+app.tsx has chain id
+
+42 is kovan
+1 is mainnet
+
+./constants
+
+    need to change those addresses
--- src/App.tsx
+++ src/App.tsx
@@ -17,121 +17,111 @@ import styles from "./app.module.scss";
 import UseWindowSize from "./components/UseWindowSize";
 
 function App() {
-  const [hasWeb3, setHasWeb3] = useState(false);
-  const [user, setUser] = useState(""); // the current connected user
-  const [isMobile, setIsMobile] = useState(false);
-  const windowSize = UseWindowSize();
-  useEffect(() => {
-    if (windowSize < 781) setIsMobile(true);
-    else setIsMobile(false);
-  }, [windowSize]);
-  useEffect(() => {
-    let isCancelled = false;
+	const [hasWeb3, setHasWeb3] = useState(false);
+	const [user, setUser] = useState(""); // the current connected user
+	const [isMobile, setIsMobile] = useState(false);
+	const windowSize = UseWindowSize();
+	useEffect(() => {
+		if (windowSize < 781) setIsMobile(true);
+		else setIsMobile(false);
+	}, [windowSize]);
+	useEffect(() => {
+		let isCancelled = false;
 
-    async function updateUserInfo() {
-      if (!isCancelled) {
-        // @ts-ignore
-        setHasWeb3(typeof window.ethereum !== "undefined");
-      }
-    }
+		async function updateUserInfo() {
+			if (!isCancelled) {
+				// @ts-ignore
+				setHasWeb3(typeof window.ethereum !== "undefined");
+			}
+		}
 
-    updateUserInfo();
-    const id = setInterval(updateUserInfo, 15000);
+		updateUserInfo();
+		const id = setInterval(updateUserInfo, 15000);
 
-    // eslint-disable-next-line consistent-return
-    return () => {
-      isCancelled = true;
-      clearInterval(id);
-    };
-  }, [user]);
-  const [homeData, setHomeData] = useState(true);
+		// eslint-disable-next-line consistent-return
+		return () => {
+			isCancelled = true;
+			clearInterval(id);
+		};
+	}, [user]);
+	const [homeData, setHomeData] = useState(true);
 
-  return (
-    <Router>
-      <UseWalletProvider
-        chainId={42}
-        connectors={{
-          walletconnect: { rpcUrl: "https://mainnet.eth.aragon.network/" },
-          walletlink: {
-            url: "https://mainnet.eth.aragon.network/",
-            appName: "Coinbase Wallet",
-            appLogoUrl: "",
-          },
-        }}
-      >
-        <Main assetsUrl={`${process.env.PUBLIC_URL}/aragon-ui/`} layout={false}>
-          {hasWeb3 ? (
-            <Switch>
-              <Route path="/dao/:override">
-                <Wallet user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/dao/">
-                <Wallet user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/epoch/">
-                <EpochDetail user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/coupons/:override">
-                <CouponMarket user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/coupons/">
-                <CouponMarket user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/governance/">
-                <Governance user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/trade/">
-                <Trade user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/regulation/">
-                <Regulation user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/pool/:override">
-                <Pool user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/pool/">
-                <Pool user={user} hasWeb3={hasWeb3} setUser={setUser} />
-              </Route>
-              <Route path="/">
-                <div className={styles.dashboard}>
-                  <div className={styles.mainBoard}>
-                    <div className={styles.container}>
-                      <HomePage
-                        hasWeb3={hasWeb3}
-                        user={user}
-                        setUser={setUser}
-                        homeData={homeData}
-                      />
-                    </div>
-                  </div>
+	return (
+		<Router>
+			<UseWalletProvider
+				chainId={1}
+				connectors={{
+					walletconnect: { rpcUrl: "https://mainnet.eth.aragon.network/" },
+					walletlink: {
+						url: "https://mainnet.eth.aragon.network/",
+						appName: "Coinbase Wallet",
+						appLogoUrl: "",
+					},
+				}}
+			>
+				<Main assetsUrl={`${process.env.PUBLIC_URL}/aragon-ui/`} layout={false}>
+					{hasWeb3 ? (
+						<Switch>
+							<Route path="/dao/:override">
+								<Wallet user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/dao/">
+								<Wallet user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/epoch/">
+								<EpochDetail user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/coupons/:override">
+								<CouponMarket user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/coupons/">
+								<CouponMarket user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/governance/">
+								<Governance user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/trade/">
+								<Trade user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/regulation/">
+								<Regulation user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/pool/:override">
+								<Pool user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/pool/">
+								<Pool user={user} hasWeb3={hasWeb3} setUser={setUser} />
+							</Route>
+							<Route path="/">
+								<div className={styles.dashboard}>
+									<div className={styles.mainBoard}>
+										<div className={styles.container}>
+											<HomePage hasWeb3={hasWeb3} user={user} setUser={setUser} homeData={homeData} />
+										</div>
+									</div>
 
-                  <div
-                    className={styles.connectWallet}
-                    style={{
-                      position:
-                        user === "" && isMobile ? "absolute" : "relative",
-                    }}
-                  >
-                    <ConnectButton
-                      user={user}
-                      setUser={setUser}
-                      setter={setHomeData}
-                    />
-                  </div>
-                </div>
-              </Route>
-            </Switch>
-          ) : (
-            <Switch>
-              <Route path="/">
-                <HomePageNoWeb3 />
-              </Route>
-            </Switch>
-          )}
-        </Main>
-      </UseWalletProvider>
-    </Router>
-  );
+									<div
+										className={styles.connectWallet}
+										style={{
+											position: user === "" && isMobile ? "absolute" : "relative",
+										}}
+									>
+										<ConnectButton user={user} setUser={setUser} setter={setHomeData} />
+									</div>
+								</div>
+							</Route>
+						</Switch>
+					) : (
+						<Switch>
+							<Route path="/">
+								<HomePageNoWeb3 />
+							</Route>
+						</Switch>
+					)}
+				</Main>
+			</UseWalletProvider>
+		</Router>
+	);
 }
 
 export default App;
